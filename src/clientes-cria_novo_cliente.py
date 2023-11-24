29d118864cffb7e075466edac30a0b76c910b7b0
nalmandoz <a79370@ualg.pt>
2023-11-24 00:11:12 +0000

    Adicionei os campos para o registo do cliente e o seu dicionário dentro de uma função no ficheiro cliente-cria_novo_cliente.py

--- src/clientes-cria_novo_cliente.py
+++ src/clientes-cria_novo_cliente.py
@@ -5,5 +5,35 @@ def cria_novo_cliente():
     """
     # TODO: pedir os dados do cliente e não esquecer de os devolver
     # ...
+    nome = input("Nome? ")
+    morada = input("Morada? ")
+    localidade = input("Localidade?")
+    c_postal = input("Código Postal? ")
+    nif = int(input("NIF? "))
+    tel = int(input("Telefone / Telemóvel? "))
+    fax = input("Fax? ")
+    email = input("Email? ")
+    doc_ident = input("Documento Identificação Nº? ")
+    emissor = input("Emissor? ")
+    validade = input("Validade (Ano)? ")
+    cacp = input("Código de Acesso a Certidão Permanente (só empresas)? ")
+    data = input("Data (YYYY-MM-DD)? ")
+    assinatura = input("Assinatura? ")
 
+    cliente = {"Nome": nome,
+               "Morada": morada,
+               "Localidade": localidade,
+               "Código Postal": c_postal,
+               "NIF": nif,
+               "Telefone ou Telemóvel": tel,
+               "Fax": fax,
+               "Email": email,
+               "Nº do documento de identificação": doc_ident,
+               "Emissor do documento de identificação": emissor,
+               "Ano de validade do documento de identificação": validade,
+               "Código de Acesso a Certidão Permanente, no caso de o cliente ser uma empresa": cacp,
+               "Data em formato YYYY-MM-DD": data,
+               "Assinatura": assinatura
+               }
     pass
+    return cliente
