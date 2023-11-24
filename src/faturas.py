1f71fe7cece12864cecf1aa886f06216198825cc
Suarez <a85242@ualg.pt>
2023-11-24 21:11:28 +0000

    apagados ficheiros faturas-*.py

--- src/faturas.py
+++ src/faturas.py
@@ -5,12 +5,6 @@ from io_terminal import imprime_lista, pergunta_id
 nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
 
 
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome faturas*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro faturas.py existente, deve apagar
-# todos os outros ficheiros faturas-*.py, e inclusive estes comentários
-
 def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
     """Pede ao utilizador para introduzir os dados de uma nova fatura
 
