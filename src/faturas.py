014628bf8ed1b4708b5651f3cea582f1ca1acd8d
Suarez <a85242@ualg.pt>
2023-11-24 11:45:45 +0000

    função que cria faturas adicionada a faturas.py

--- src/faturas.py
+++ src/faturas.py
@@ -11,4 +11,20 @@ nome_ficheiro_lista_de_faturas = "lista_de_faturas.pk"
 # deve ser o unico ficheiro faturas.py existente, deve apagar
 # todos os outros ficheiros faturas-*.py, e inclusive estes comentários
 
-# ...
+def cria_nova_fatura(lista_de_clientes, lista_de_veiculos):
+    """Pede ao utilizador para introduzir os dados de uma nova fatura
+
+    :return: dicionario com uma fatura, na forma
+        {"cliente": <<id_cliente>>, "veiculo": <<id_veiculo>>, "data": <<data>>, ...}
+    """
+
+    id_cliente = pergunta_id(questao="Qual o id do cliente?", lista=lista_de_clientes, mostra_lista=True)
+    id_veiculo = pergunta_id(questao="Qual o id do veiculo?", lista=lista_de_veiculos, mostra_lista=True)
+    desc = input('Descrição da fatura: ')
+
+    fatura = {"cliente": id_cliente,
+              "veiculo": id_veiculo,
+              "data": date.today(),
+              "desc": desc}
+
+    return fatura
