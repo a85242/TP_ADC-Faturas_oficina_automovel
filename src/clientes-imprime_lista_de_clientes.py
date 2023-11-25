c006e0d1ab57ed04a40d8a69d80eef8a84d26105
nalmandoz <a79370@ualg.pt>
2023-11-25 00:41:40 +0000

    Eliminei o ficheiro porque o seu conteúdo já foi colocado no ficheiro clientes.py

--- src/clientes-imprime_lista_de_clientes.py
+++
@@ -1,7 +0,0 @@
-def imprime_lista_de_clientes(lista_de_clientes):
-    """Imprime a lista de clientes
-    
-    :param lista_de_veiculos: Lista de clientes com o seu dicionario.
-    :type lista_de_veiculos: list
-    """
-    imprime_lista(cabecalho="Lista de Clientes", lista=lista_de_clientes)
