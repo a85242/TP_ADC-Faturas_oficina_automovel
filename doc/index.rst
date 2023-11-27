fc5c61f5374f38268e6aa7bb5eb30c2ea706f689
Suarez <a85242@ualg.pt>
2023-11-27 11:07:25 +0000

    ficheiros atualizados de acordo com main

--- doc/index.rst
+++ doc/index.rst
@@ -10,7 +10,7 @@ Welcome to Trabalho_ADC's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-
+   modules
 
 Indices and tables
 ==================
--- src/io_ficheiros.py
+++ src/io_ficheiros.py
@@ -1,10 +1,3 @@
-# TODO: Copie para aqui o código de cada uma das funções nos
-# ficheiros com o nome io_ficheiros*.py e faça um commit de cada vez
-# Quando este ficheiro estiver completo com todas as suas funções,
-# deve ser o unico ficheiro io_ficheiros.py existente, deve apagar
-# todos os outros ficheiros io_ficheiros-*.py, e inclusive estes comentários
-# ...
-
 import pickle
 
 from clientes import nome_ficheiro_lista_de_clientes
